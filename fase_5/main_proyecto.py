f18a9459b1109e745261186be6f7d1f5d71ddbb8
Margmtr <marg_mtr92@hotmail.com>
2024-06-10 21:33:33 +0200

    etl, fase 2 terminada

--- fase_5/main_proyecto.py
+++ fase_5/main_proyecto.py
@@ -1,3 +1,4 @@
+
 #%%
 import os
 from dotenv import load_dotenv
@@ -9,59 +10,108 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.impute import KNNImputer
 import re
-
+from word2number import w2n
 # %%
 print('===============================================================\n')
-print('EDA\n')
+print('                             EDA\n')
 print('===============================================================\n')
 
 #%%
 #Importamos csv.
-
 url1=('https://raw.githubusercontent.com/s-armeni/proyecto-da-promo-H-modulo-3-team-3-DataMinds/main/HR%20RAW%20DATA.csv')#!utilizamos url para que sea reproducible en otros ordenadores
-
 # %%
 df_data=ps.import_csv(url1)
 #Visualizamos el número total de filas y de columnas
 print(f'Cantidad de filas: {df_data.shape[0]}\nCantidad de columnas: {df_data.shape[1]}')
-
-
 #%%
 #limpiamos las columnas.
 #Al haberlas arreglado parcialmente a mano, importamos la lista con las nuevas columnas.
 data = open("columns_handmade.txt", "r")#Esta funcion no es parte del soporte por lo que no ha<cer falta que lleve.api
 new_columns = list(data)
-print(new_columns)
-
 #%%
-#Introducimos las columnas en en el df.
-def rename_columns(df, new_col):
-    ''' Limpiamos lista de columnas.Renombramos las columnas por valores predefinidos
-    args:   (df) Df a modificar
-            (list) Lista con una string donde se encuentran los nuevos nombres columnas
-    return:  (df) Df con las columnas modificadas.'''
-    old_columns=df.columns
-    lista_new_col1=re.sub("'","", new_col[0])
-    lista_new_col=re.split(',', lista_new_col1)
-    lista_new=[]
-    for i in lista_new_col:
-        lista_new.append(i.strip())
-    print(lista_new)
-    d=zip(old_columns, lista_new)
-    df.rename(columns=d, inplace=True)
-    return df
-#%%
-rename_columns(df_data,new_columns)
-df_data.head(1)
-
-
-
-#%%
-#nos aseguramos que la lista tiene el numero de columnas correctas:
-df_data=ps.check_columns(new_columns)
+ps.rename_columns(df_data,new_columns)
 # %%
 def capitalize(df):
     df.columns=df.columns.str.capitalize()
     return df
+capitalize(df_data)
+# %%
+df_data['Age']=df_data['Age'].apply(ps.age_to_int)#Estandarización columna 'Age' 
+# %%
+df_data.loc[df_data['Distance_from_home']<0, 'Distance_from_home']*=-1#Eliminamos los '-' de 'Distance_from_home'
+df_data['Marital_status']=df_data['Marital_status'].replace('Marreid','married')#Corregimos errores ortográficos en la columna 'Marital_status'
+df_data['Environment_satisfaction']=df_data['Environment_satisfaction'].apply(ps.extrapolate)#Aplicamos la funcion anterior a la columna 'Environment_satisfaction'
+# %%
+#Función para eliminar los $
+list_dollar=['Daily_rate', 'Salary']
+df_data=ps.remove_dollar(df_data, list_dollar)
+# %%
+#Para poder operar con la media y transformalos en float, es necesario pasar el 'not available' a valores nulo nan:
+df_data['Hourly_rate']=df_data['Hourly_rate'].apply(ps.change_to_null)
+# %%
+#*Agrupamos las columnas por tipo de limpieza/correción que necesitan.
+#Lista de columnas integer a object hombre/mujer
+list_male_female=['Gender']
+#Lista de columnas yes/no
+list_yes_no=['Attrition', 'Over_18', 'Over_time', 'Remote_work']
+#Lista de columnas integer a float
+list_int_float=['Distance_from_home', 'Employee_count', 'Monthly_rate', 'Percent_salary_hike']
+#Lista de columnas object a float
+list_object_float=['Daily_rate', 'Hourly_rate', 'Monthly_income', 'Standard_hours', 'Same_as_monthly_income', 'Salary']
+#Lista de columnas object a integer
+list_object_integer=['Employee_number', 'Performance_rating', 'Total_working_years', 'Work_life_balance', 'Years_in_current_role']
+# %%
+# Eliminamos posibles espacios en blanco al principio y final de las celdas.
+df_data=df_data.applymap(ps.strip_strings) #.applymap() porque lo aplico a cada elemento del df
+# %%
+#Estandarizamos la columna de género.
+df_data=ps.male_female(df_data, 'Gender')
+# %%
+#Estandarizamos las columnas de 'Yes/No'.
+df_data=ps.unify_yes_no(df=df_data, columns=list_yes_no)
+# %%
+#Estandarizamos columnas integer a float.
+df_data=ps.int_to_float(df_data, list_int_float)
+# %%
+# Estandarizamos columnas object a float.
+df_data=ps.object_to_float(df_data, list_object_float)
+# %%
+#Estandarizamos columnas object a integer.
+df_data[['Employee_number', 'Performance_rating', 'Total_working_years', 'Work_life_balance', 'Years_in_current_role']] = df_data[['Employee_number', 'Performance_rating', 'Total_working_years', 'Work_life_balance', 'Years_in_current_role']].replace(',0', '', regex=True).astype(float).astype(pd.Int64Dtype()) #esta parte permite convertir a integer y que se mantengan los nulos existentes
+# %%
+#Unificamos todos los textos
+cat_cols=df_data.select_dtypes(include=['O'])
+df_data=ps.capitalize_string(df_data, cat_cols)
 
 # %%
+#Gestión de nulos y eliminación de columnas prescindibles
+cat_null,num_null=ps.null_percentage(df_data)
+# %%
+'''Observando los nulos y sus %, decidimos eliminar las siguientes columnas:
+Department 81.29% nulos.
+Role_departament 81.29% nulos.'''
+df_data.drop(['Department', 'Role_departament'], axis=1, inplace=True)
+
+# %%
+'''Observando los altos % de nulos en el resto de columnas categóricas, decidimos sustituir los valores NaN por 'Unknown' para no alterar la veracidad de los datos.'''
+ps.unknown_data(df_data)
+# %%
+df_data.drop(['Monthly_income', 'Standard_hours', 'Years_in_current_role', 'Same_as_monthly_income', 'Number_children', 'Salary'], axis=1, inplace=True)
+# %%
+#Columna Employee_number. Para que la empresa pueda seguir utilizando este identificador a nivel interno:
+#Convertimos la columna a object
+df_data['Employee_number']=df_data['Employee_number'].astype('object')
+#Sustituimos los nullos por 'unkown'.
+df_data['Employee_number']=df_data['Employee_number'].fillna('Unknown')
+# %%
+#Para las columnas con un % de nulos inferior al 10%:
+#Reemplazamos nulos con la media
+columns_mean=['Daily_rate', 'Hourly_rate', 'Work_life_balance']
+ps.media_nulos(df_data,columns_mean)
+#%%
+#Reemplazamos nulos con IterativeImputer
+columns_iterative_imputer=['Performance_rating', 'Total_working_years']
+ps.iterative_nulos(df_data,columns_iterative_imputer)
+#%%
+df_data.to_csv('hr_data_final_etl.csv')
+
--- fase_5/src_proyecto/proyecto_soporte.py
+++ fase_5/src_proyecto/proyecto_soporte.py
@@ -9,6 +9,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.impute import KNNImputer
 import re
+from word2number import w2n
 # %%
 def import_csv(url):
     ''' importa csv desde la carpeta de trabajoy lo guarda en un dataframe.
@@ -19,8 +20,14 @@ def import_csv(url):
     '''
     df_raw=pd.read_csv(url) #importamos csv
     return df_raw.iloc[:, 1:] #seleccionamos solo las columnas que nos interesan.
+
 # %%
-def cleck_columns (df, new_columns):
+def check_columns (df, new_columns):
+    ''' Checkea las columnas que se introducen manualmente para comprobar que están todas y cambia los nombres en el df antiguo.
+        args:   (df) Df del cual se desea hacer los calculos.
+                (list) Lista de columnas nuevas.
+        return:  (df) Df con los nombres de las columnas actualizados.
+    '''
     #Hacemos un if para asegurarnos que no se queda ninguna columna sin nombre
     if len(new_columns)==len(df.columns):
         df.columns=new_columns
@@ -29,25 +36,62 @@ def cleck_columns (df, new_columns):
         raise ValueError('La lista new_columns debe tener la misma longitud que número de columnas tiene el DataFrame.')
 
 #%%
+#Introducimos las columnas en en el df.
+def rename_columns(df, new_col):
+    ''' Limpiamos lista de columnas.Renombramos las columnas por valores predefinidos
+    args:   (df) Df a modificar
+            (list) Lista con una string donde se encuentran los nuevos nombres columnas
+    return:  (df) Df con las columnas modificadas.
+    '''
+    old_columns=df.columns
+    lista_new_col1=re.sub("'","", new_col[0])
+    lista_new_col=re.split(',', lista_new_col1)
+    lista_new=[]
+    for i in lista_new_col:
+        lista_new.append(i.strip())
+    d=dict(zip(old_columns, lista_new))
+    df.rename(columns=d, inplace=True)
+    return df
+
+#%%
 def capitalize(df):
+    ''' Pasa la primera letra de los titulos de las columnas a mayúscula
+    args:   (df) Df a modificar
+    return:  (df) Df con las columnas modificadas.
+    '''
     df.columns=df.columns.str.capitalize()
     return df
-# %%
-def age_to_int(num):
-    try:
-        return int(num)
-    except ValueError:
-        try:
-            return w2n.word_to_num(num)
-        except ValueError:
-            return np.nan
 
 # %%
-def remove_caracter(df,columna):
+def age_to_int(num):
+    ''' modifica aquellos valores que expresan numeros en una string y los pasa a dígitos. Si encuentra valores nulos los pasa a formato np.nan
+    args:   (str) str a modificar
+    return:  (int64) dígito.
+    '''
+    try:
+        return int(num) #Devuelve el digito en formato intenger.
+    except ValueError:
+        try:
+            return w2n.word_to_num(num) #usamos la función word_to_num() de la libreria word2number
+        except ValueError:
+            return np.nan # Transformamos los nulos en un formato válido.
+
+# %%
+def remove_negative_values(df,columna):
+    '''Transforma los valores negativos en positivos
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.
+    return:  (df) Df con las columnas modificadas.
+    '''
     df.loc[df[columna]<0, columna]*=-1
     return df
+
 # %%
 def extrapolate(valor):
+    '''Categoriza los valores en funcion de su valor en cuatro tipos.
+    args:   (int) valor a categorizar       
+    return:  (int)categoría a la que corresponde en función de su valor.
+    '''
     if 4>=valor:
         return valor
     elif 4<valor<=10:
@@ -60,50 +104,165 @@ def extrapolate(valor):
         return 3
     elif valor>40:
         return 4
+    
 # %%
 def remove_dollar(df, columns):
+    '''Remueve el simbolo $ de una string
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.       
+    return:  (df) Df con las columnas modificadas.
+    '''
     for i in columns:
         df[i]=df[i].str.replace('$', '', regex=False)
     return df
 # %%
 def change_to_null (string):
+    '''Cambia los valores 'Not Available' por valores nulos en un formato válido.
+    args:   (str) valor a modificar      
+    return:  (str) Valor nulo en formato válido.
+    '''
     if string == 'Not Available':
         return np.nan
     else:
         return string
 # %%
 def strip_strings(cell):
+    '''Elimina los espacios al principio y final de una string.
+    args:   (str) valor a modificar      
+    return:  (str) string sin espacios.
+    '''
     if isinstance(cell, str): #si es un string retorna sin espacios
         return cell.strip()
     return cell
+
 # %%
 def male_female(df, column):
+    '''transforma los 0 en 'Male' y los 1 en 'Female'
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.       
+    return:  (df) Df con las columnas modificadas.
+    '''
     df[column]=df[column].replace({0:'Male', 1:'Female'})
     return df
+
 # %%
 def unify_yes_no(df, columns):
+    '''unifica los valores y los clasifica en Yes/No'
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.       
+    return:  (df) Df con los valores modificados.
+    '''
+
     yes_no_map={'yes':'Yes', 'YES':'Yes', 'TRUE':'Yes', 'true':'Yes', '1':'Yes', 'Y':'Yes', 'Yes':'Yes',
         'no':'No', 'No':'No', 'NO':'No', 'FALSE':'No', 'false':'No', '0':'No', 'False':'No'}
     
     for i in columns:
         df[i]=df[i].astype(str).map(yes_no_map)
     return df
+
 # %%
 def int_to_float(df, columns):
+    '''Transforma el tipo de valor intenger a float de columnas específicas'
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.       
+    return:  (df) Df con los valores modificados.
+    '''
     for i in columns:
         df[i]=df[i].astype(float).round(2)
     return df
 # %%
 def object_to_float(df, columns):
+    '''Transforma el tipo de valor object a float de columnas específicas. Cambia las ',' por '.''
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.       
+    return:  (df) Df con los valores modificados.
+    '''
     for i in columns:
         try:
             df[i]=df[i].str.replace(',', '.').astype(float).round(2)
         except:
             df[i]=df[i].astype(float).round(2)
     return df
+
 # %%
-def capitalize_string(df, columns): #! REPETIDO ESTE CODIGO EN LA LIMPIEZA NOMBRES COLUMNA
+def capitalize_string(df, columns):
+    ''' Pasa la primera letra de los titulos de las columnas a mayúscula de columnas específicas.
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.
+    return:  (df) Df con las columnas modificadas.
+    '''                                       #! REPETIDO ESTE CODIGO EN LA LIMPIEZA NOMBRES COLUMNA
     for i in columns:
         df[i]=df[i].str.capitalize()
     return df
+
 # %%
+#Gestión de nulos y eliminación de columnas prescindibles
+def null_percentage(df):
+    ''' Calcula el porcentaje de valores nulos en las columnas del dataframe.
+    args:   (df) Df a modificar
+            
+    return: (df) Porcenajes de valores nulos por columnas categóricas.
+            (list) Porcenajes de valores nulos por columnas numéricas.
+    ''' 
+    #Separamos las columnas por categóricas y numéricas
+    cat_cols=df.select_dtypes(include=['O'])
+    num_cols=df.select_dtypes(include=['number'])
+
+    #Calculamos la cantidad y % de nulos de las columnas categóricas
+    cat_nulos_df=pd.DataFrame({'Column': cat_cols.columns, #trae los nombres de las columnas
+                                'Nulos': cat_cols.isnull().sum(), #trae la suma de los nulos
+                                '% Nulos': (cat_cols.isnull().mean() * 100).round(2)}) #trae el % de nulos
+    cat_nulos_df=cat_nulos_df[cat_nulos_df['Nulos'] > 0] #filtra para aquellos >0
+
+    #Calculamos la cantidad y % de nulos de las columnas numéricas
+    num_nulos_df=pd.DataFrame({'Column': num_cols.columns, #trae los nombres de las columnas
+                                'Nulos': num_cols.isnull().sum(), #trae la suma de los nulos
+                                '% Nulos': (num_cols.isnull().mean() * 100).round(2)}) #trae el % de nulos
+
+    #Creamos variables en las que guardamos las medias y medianas de cada columna numérica
+    num_mean=num_cols.mean()
+    num_median=num_cols.median()
+    #Añadimos las medias y medianas al df de columnas numéricas
+    num_nulos_df['Mean']=num_nulos_df['Column'].map(num_mean)
+    num_nulos_df['Median']=num_nulos_df['Column'].map(num_median)
+
+    num_nulos_df=num_nulos_df[num_nulos_df['Nulos'] > 0] #filtra para aquellos >0
+    return cat_nulos_df, num_nulos_df
+# %%
+
+def unknown_data (df): 
+    ''' Transforma los valores nulos en el string 'Unknow'
+    args:   (df) Df a modificar
+            
+    return: (df) Df con los valores modificados
+    '''
+    cat_cols=df.select_dtypes(include=['O'])
+    for i in cat_cols:
+        df[i]=df[i].fillna('Unknown')
+    return df
+
+# %%
+def media_nulos(df, columns):
+    ''' Sustituye los valores nulos de columnas específicas por el promedio del los datos de la misma.
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.
+    return:  (df) Df con las columnas modificadas.
+    ''' 
+    for i in columns:
+        imputer=SimpleImputer(strategy='mean')
+        df[i]=imputer.fit_transform(df[[i]])
+        df[i]=round(df[i], 2)
+    return df
+
+# %%
+def iterative_nulos(df, columns):
+    ''' Sustituye los valores nulos de columnas específicas por un valor estimado mediante el método estadístico Interative Imputer
+    args:   (df) Df a modificar
+            (list) Lista de columnas a modificar.
+    return:  (df) Df con las columnas modificadas.
+    ''' 
+    for i in columns:
+        imputer=IterativeImputer(max_iter=20, random_state=42)
+        df[i]=imputer.fit_transform(df[[i]])
+        df[i]=round(df[i], 2)
+    return df
